345bc8db53520e64dcb853d50a33e0636f0475e7
mahdy151 <mahdy151saadat@gmail.com>
2023-01-31 02:27:06 +0330

    homeee

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-    import '@picocss/pico';
+	import '@picocss/pico'
 </script>
+
 <nav>
-    <ul>
-        <li>
-            <a href="/">HOME</a>
-        </li>
-        <li>
-            <a href="/posts">POST</a>
-        </li>
-    </ul>
+	<ul>
+		<li>
+			<a href="/">HOME</a>
+		</li>
+		<li>
+			<a href="/posts">POST</a>
+		</li>
+	</ul>
 </nav>
 
-<h1>HOME PAGE</h1>
+<h1>HOMEEEE PAGE</h1>
