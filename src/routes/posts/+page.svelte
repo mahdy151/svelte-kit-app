ae68aa9f7dbc02203e529267fb28bd0c6dd65ac2
mahdy151 <mahdy151saadat@gmail.com>
2023-01-31 01:16:22 +0330

    first

---
+++ src/routes/posts/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import '@picocss/pico';
+</script>
+<nav>
+    <ul>
+        <li>
+            <a href="/">HOME</a>
+        </li>
+        <li>
+            <a href="/posts">POST</a>
+        </li>
+    </ul>
+</nav>
+
+<h1>POSTS PAGE</h1>
---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from 'svelte-adapter-bun'
+import preprocess from 'svelte-preprocess'
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter({
+			out: 'build',
+			assets: true,
+			dynamic_origin: true
+		})
+	}
+}
+
+export default config
